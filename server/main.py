9a39b6d00f5e3a9b7b8ede5092784fb28cbf64f9
vladmialkin <myalkin.vlad@yandex.ru>
2025-02-11 14:13:43 +0300

    feat: Получение параметров бд из .env.

--- server/main.py
+++ server/main.py
@@ -1,12 +1,16 @@
 from server import Server
 import asyncio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 db_config = {
-    "user": "postgres",
-    "password": "postgres",
-    "database": "postgres",
-    "host": "127.0.0.1",
-    "port": 5432,
+    "user": os.getenv("POSTGRES_USER"),
+    "password": os.getenv("POSTGRES_PASSWORD"),
+    "database": os.getenv("POSTGRES_DB", "postgres"),
+    "host": os.getenv("POSTGRES_HOST", "127.0.0.1"),
+    "port": int(os.getenv("POSTGRES_PORT", 5432)),
 }
 
 if __name__ == '__main__':
